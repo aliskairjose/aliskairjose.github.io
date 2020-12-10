cf1e2dd5a39473f00eedcdc21db3d344f9c4e49c
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-12-09 21:13:34 -0400

    add token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   api_key: 'e7ea8addcdf47faebaaec717da10ddcc',
-  url: 'https://api.themoviedb.org'
+  url: 'https://api.themoviedb.org',
+  token: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlN2VhOGFkZGNkZjQ3ZmFlYmFhZWM3MTdkYTEwZGRjYyIsInN1YiI6IjVmZDE2NWYwMGNlMWJjMDA0MGYxZjQwZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NJnMPxq18bl0wtsouG6Gqdt9qE2_r_dShhaGOT8rLXA'
 };
 
 /*
