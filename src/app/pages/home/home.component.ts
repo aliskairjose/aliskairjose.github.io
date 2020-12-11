3ccecda9e801c90e1b957e34c6f58149f29db46d
Kervin Gonzalez <kervingonzalez@gmail.com>
2020-12-10 23:03:22 -0400

    ajsute de fondo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { forkJoin } from 'rxjs';
 import { ApiService } from '../../shared/services/api.service';
 import { Movie } from '../../shared/interfaces/movie.interface';
@@ -11,7 +11,7 @@ import { environment } from '../../../environments/environment.prod';
   templateUrl: './home.component.html',
   styleUrls: [ './home.component.scss' ]
 } )
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
   movies: Movie[] = [];
   tvShows: TV[] = [];
@@ -36,8 +36,12 @@ export class HomeComponent implements OnInit {
         this.isShowing = false;
       } );
   }
+  ngOnDestroy(): void {
+    document.body.style.setProperty( 'background-color', '#373737', 'important' );
+  }
 
   ngOnInit() {
+    document.body.style.setProperty( 'background-color', 'black', 'important' );
   }
 
 }
